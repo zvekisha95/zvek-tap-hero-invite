b9186a5b15854ca394982f7645ce6e46a3ef211a
zvekisha95 <dragan.priest@gmail.com>
2025-11-10 19:27:12 +0100

    Update log.js

--- api/log.js
+++ api/log.js
@@ -1,6 +1,10 @@
-export default () => {
+// /api/log.js
+
+export default function handler() {
+  globalThis.inviteLog = globalThis.inviteLog || [];
+
   return new Response(
-    JSON.stringify(globalThis.inviteLog || []),
-    { status: 200 }
+    JSON.stringify(globalThis.inviteLog, null, 2),
+    { status: 200, headers: { "Content-Type": "application/json" } }
   );
-};
+}
